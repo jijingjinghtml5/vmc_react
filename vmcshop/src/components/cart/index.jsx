bd4de2814e4adc5fab9b37747ce646e300a564a2
jijingjing <jijingjing@vmchop.com>
2019-06-21 18:37:20 +0800

    vcmshop

--- vmcshop/src/components/cart/index.jsx
+++ vmcshop/src/components/cart/index.jsx
@@ -1,23 +1,105 @@
 import React,{Component} from 'react';
-import { NavBar, Icon } from 'antd-mobile';
+import { Toast,Modal,Icon } from 'antd-mobile';
+import { Tool,Util } from '../../common/util';
 import Header from "../../common/Header";
 import Footer from "../../common/Footer";
 import PropTypes from 'prop-types';
+import "./cart.less";
 
+const alert = Modal.alert;
 export default class Cart extends Component{
     constructor(props) {
         super(props);
-        this.goLogin = this.goLogin.bind(this)
+        this.state = {
+            images:{}
+        }
+        Cart._this = this;
     }
-    goLogin(){
-        console.log(this.context.history);
-        this.context.history.push('/pages/login/login')
+    componentDidMount(){
+        this.getData();
+    }
+    loadImage=(image_id)=>{
+        console.log(image_id);
+        Util.loadImage(this,image_id,'m');
+    }
+    //移动
+    drawStart=(id,event)=>{
+        console.log(11111);
+        this.setState({
+            touchId:0,
+            touchX:event.touches[0].pageX
+        })
+    }
+    drawMove=(id,event)=>{
+        let moveX = this.state.touchX - event.touches[0].pageX;
+        if (moveX > 30) {
+            this.setState({
+                touchId:id
+            })
+        } else {
+            this.setState({
+                touchId:0
+            })
+        }
+    }
+    //删除
+    evt_delete=(ident,event)=>{
+        // event.stopPropagation();
+        // event.nativeEvent.stopImmediatePropagation();
+        if (!ident) {
+            return;
+        }
+        alert('温馨提示', '确定要从购物车里删除???', [
+            { text: '取消', onPress: () => {
+                // console.log('cancel');
+            } },
+            { text: '确认', onPress: async () => {
+                let res = await Tool.post('/m/cart-remove-' + ident + '.html',{});
+                if(res.success){
+                    Toast.success('删除成功', 2, function(){
+                        Cart._this.getData();
+                    }, true);
+                }
+            } },
+        ])
+    }
+    getData = async () => {
+        let res = await Tool.post('/m/cart.html',{});
+        if(res.success){
+            this.setState({...res.data});
+        }
     }
     render(){
+        let images = this.state.images;
         return (
             <div>
                 <Header title={"购物车"} isBack={false}></Header>
-                <div onClick={this.goLogin}>去登录页</div>
+                <ul>
+                    {
+                        this.state&&this.state.objects?(
+                            this.state.objects.map((val,index)=>(
+                                <li className={'merchant-goods'} key={val.merchant.merchant_id}>
+                                    <div className={'merchant-name'}>
+                                        <label></label>
+                                        <span>{val.merchant.show_name}</span>
+                                    </div>
+                                    {
+                                        val.objects.goods.map((obj,idx)=>(
+                                            <div key={obj.obj_ident} className={Cart._this.state.touchId==obj.obj_ident?'select_li merchant-item':'merchant-item'} onTouchStart={this.drawStart.bind(this,obj.obj_ident)} onTouchMove={this.drawMove.bind(this,obj.obj_ident)} onTouchEnd={this.drawEnd}>
+                                                <label></label>
+                                                <img className={'goods-img'} src={images[obj.item.product.image_id]?images[obj.item.product.image_id]:'data:image/gif;base64,R0lGODlhAQABAIAAAO/v7////yH5BAAHAP8ALAAAAAABAAEAAAICRAEAOw=='} 
+                                onLoad={this.loadImage.bind(this,obj.item.product.image_id)} />
+                                                <div>{obj.item.product.name}</div>
+                                                <div onClick={this.evt_delete.bind(this,obj.obj_ident)} className={Cart._this.state.touchId==obj.obj_ident?'goods-delete':'goods-delete'}>删除</div>
+                                            </div>
+                                        ))
+                                    }
+                                </li>
+                            ))
+                        ):''
+                        
+                    }
+                </ul>
                 <Footer selectedTab="cart"></Footer>
             </div>
         )
--- vmcshop/src/components/cat/index.jsx
+++ vmcshop/src/components/cat/index.jsx
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import { Drawer, List, NavBar, Icon } from 'antd-mobile';
+import { Tool,Util } from '../../common/util';
 import Header from "../../common/Header";
 import Footer from "../../common/Footer";
 import PropTypes from 'prop-types';
@@ -16,20 +17,33 @@ export default class Cat extends Component{
         console.log(args);
         this.setState({ open: !this.state.open });
     }
+    change_cat=(id)=>{
+        console.log(id);
+    }
+    componentDidMount(){
+        this.getData();
+    }
+    getData = async () => {
+        let res = await Tool.post('/m/category.html',{});
+        if(res.category_tree){
+            this.setState({...res});
+            console.log(this.state.category);
+        }
+    }
     render(){
-        const sidebar = (<List>
-           {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i, index) => {
-            if (index === 0) {
-              return (<List.Item key={index}
-                thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-                multipleLine
-              >Category</List.Item>);
-            }
-            return (<List.Item key={index}
-              thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-            >Category{index}</List.Item>);
+        const sidebar = this.state.category_tree?(<List>
+           {this.state.category_tree.map((i, index) => {
+            // if (index === 0) {
+            //   return (<List.Item key={index}
+            //     thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+            //     multipleLine
+            //   >Category</List.Item>);
+            // }
+            return (<List.Item onClick={this.change_cat.bind(this,i.cat_id)} key={index}
+              // thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+            >{i.cat_name}</List.Item>);
           })}
-        </List>);
+        </List>):'';
         return (
             <div>
                 <Header title={"分类"} isBack={false}></Header>
